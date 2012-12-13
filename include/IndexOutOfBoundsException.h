2cf40f033bad76522de93bae92be4119e32ea4e9
Izzo <hristoz2004@gmail.com>
2012-12-13 15:47:38 +0000

    Added an IndexOutOfBoundsException 'type'@

---
+++ include/IndexOutOfBoundsException.h
@@ -0,0 +1,19 @@
+#ifndef __VROOM_INDEXOUTOFBOUNDS_H__
+#define __VROOM_INDEXOUTOFBOUNDS_H__
+
+
+
+
+
+//=================================================================
+//	struct IndexOutOfBoundsException
+//---------------------------------------
+struct IndexOutOfBoundsException
+{
+};
+
+
+
+
+
+#endif
