f8444a01e06d723cff5ed608162d0b5e2c1cfd7d
Izzo <hristoz2004@gmail.com>
2012-12-14 21:16:53 +0000

    Empty class and test added for the slice renderer

---
+++ include/TexMapVolumeRenderer.h
@@ -0,0 +1,28 @@
+#ifndef __VROOM_TEXMAP_VOLUME_RENDERER_H__
+#define __VROOM_TEXMAP_VOLUME_RENDERER_H__
+
+
+
+
+
+//=================================================================
+//	Inlude
+//---------------------------------------
+#include <Volume.h>
+
+
+
+
+
+//=================================================================
+//	Class TexMapVolumeRenderer
+//---------------------------------------
+class TexMapVolumeRenderer
+{
+}
+
+
+
+
+
+#endif
---
+++ test/TestTexMapVolumeRenderer.cpp
@@ -0,0 +1,9 @@
+#include <UnitTest++.h>
+
+SUITE( TexMapVolumeRenderer )
+{
+	TEST( TEST_renderer_can_render_opaque_objects_correctly )
+	{
+
+	}
+}
