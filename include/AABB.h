7606308b9c43f5fb9484b38d1e44ad743822c929
Izzo <hristoz2004@gmail.com>
2012-12-17 09:21:05 +0000

    Added an AABB header

---
+++ include/AABB.h
@@ -0,0 +1,25 @@
+#ifndef __VROOM_AABB_H__
+#define __VROOM_AABB_H__
+
+
+
+
+
+//=================================================================
+//	Inlude
+//---------------------------------------
+#include <Platform.h>
+
+
+VROOM_BEGIN
+
+
+class AABB
+{
+};
+
+
+VROOM_END
+
+
+#endif
