817f315c2249087b49de7ceed472562e273d29df
Hristoz Stefanov <hristoz2004@gmail.com>
2013-03-30 11:26:34 +0000

    Added an invalid state excepion

---
+++ include/InvalidStateException.h
@@ -0,0 +1,25 @@
+#ifndef __VROOM_INVALIDSTATE_H__
+#define __VROOM_INVALIDSTATE_H__
+
+
+//=================================================================
+//	Inlude
+//---------------------------------------
+#include <Platform.h>
+
+
+VROOM_BEGIN
+
+
+//=================================================================
+//	struct InvalidStateException
+//---------------------------------------
+struct InvalidStateException
+{
+};
+
+
+VROOM_END
+
+
+#endif
